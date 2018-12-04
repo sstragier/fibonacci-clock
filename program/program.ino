f126371552d5dff9207d9f67d1aafdda29e847c2
Steven <steve.stragier@gmail.com>
2018-12-03 22:34:03 -0800

    Fixed compile issues

--- program/program.ino
+++ program/program.ino
@@ -1,6 +1,8 @@
-#include "FastLED.h"
+#include <Arduino.h>
 #include <RTClib.h>
+#include "FastLED.h"
 #include <Wire.h>
+
 #include "colors.h"
 
 #define NUM_LEDS 10
@@ -26,255 +28,370 @@ int prevMinute = 9999;
 int prevButtonValue[MAX_BUTTONS_INPUT];
 int currentButtonValue[MAX_BUTTONS_INPUT];
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     Serial.println("starting");
-    
+
     setupLeds();
     setupClock();
     setupButtons();
 }
 
-void setupLeds() {
+void setupLeds()
+{
     FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS);
 }
 
-void setupButtons() {
+void setupButtons()
+{
     pinMode(BUTTON_PIN, INPUT);
     pinMode(SET_PIN, INPUT);
     pinMode(HOUR_PIN, INPUT);
     pinMode(MINUTE_PIN, INPUT);
 }
 
-void setupClock() {
+void setupClock()
+{
     Wire.begin();
     rtc.begin();
 
-    if (rtc.lostPower()) {
+    if (rtc.lostPower())
+    {
         Serial.println("RTC lost power, lets set the time!");
-        setTime(DateTime(F(__DATE__), F(__TIME__)));
+        //setTime(DateTime(F(__DATE__), F(__TIME__)));
     }
 }
 
-void loop() {
+void loop()
+{
 
-  int hour_button = debounce(HOUR_PIN);
-  int minute_button = debounce(MINUTE_PIN);
+    int hour_button = debounce(HOUR_PIN);
+    int minute_button = debounce(MINUTE_PIN);
 
-  DateTime now = rtc.now();
-  
-  if (hour_button && hasChanged(HOUR_PIN)) {
-    setTime(DateTime(now.unixtime() + 3600));
-  } else if (minute_button && hasChanged(MINUTE_PIN)) {
-    // Only increase the minutes, don't also increase the hours
-    setTime(DateTime(now.year(), now.month(), now.day(), now.hour(), (now.minute() + 1) % 60));
-  }
-  
-  resetButtonValues();
-  
-  int hour = now.hour() % 12;
-  int minute = now.minute() / 5;
-  
-  if (hour == prevHour && minute == prevMinute) return;
-  printDateTime(now);
-  Serial.print("Hour: "); Serial.print(hour, DEC); Serial.print(" Minute: "); Serial.print(minute, DEC); Serial.println();
+    DateTime now = rtc.now();
 
-  prevHour = hour;
-  prevMinute = minute;
+    if (hour_button && hasChanged(HOUR_PIN))
+    {
+        setTime(DateTime(now.unixtime() + 3600));
+    }
+    else if (minute_button && hasChanged(MINUTE_PIN))
+    {
+        // Only increase the minutes, don't also increase the hours
+        setTime(DateTime(now.year(), now.month(), now.day(), now.hour(), (now.minute() + 1) % 60));
+    }
 
-  resetClockSquareFlags();
-  setClockSquareFlags(hour, FLAG_HOUR_ON);
-  setClockSquareFlags(minute, FLAG_MINUTE_ON);
+    resetButtonValues();
 
-  for (int i = 0; i < NUM_CLOCK_SQUARES; i++) {
-    updateLed(i, clockSquares[i]);
-  }
-  FastLED.show();
+    int hour = now.hour();
+    if (hour > 12)
+        hour %= 12;
+    int minute = now.minute() / 5;
+
+    if (hour == prevHour && minute == prevMinute)
+        return;
+    printDateTime(now);
+    Serial.print("Hour: ");
+    Serial.print(hour, DEC);
+    Serial.print(" Minute: ");
+    Serial.print(minute, DEC);
+    Serial.println();
+
+    prevHour = hour;
+    prevMinute = minute;
+
+    resetClockSquareFlags();
+    setClockSquareFlags(hour, FLAG_HOUR_ON);
+    setClockSquareFlags(minute, FLAG_MINUTE_ON);
+
+    for (int i = 0; i < NUM_CLOCK_SQUARES; i++)
+    {
+        updateLed(i, clockSquares[i]);
+    }
+    FastLED.show();
 }
 
 int debounce(int pin)
 {
-  int val = digitalRead(pin);
-  if (val == prevButtonValue[pin])
-  {
-    currentButtonValue[pin] = val;
-    return val;
-  }
-    
-  delay(DEBOUNCE_DELAY);
-  
-  val = digitalRead(pin);
-  if(val != prevButtonValue[pin])
-  {
-    Serial.print(pin, DEC); Serial.print(" is "); Serial.println(val, DEC);
-    currentButtonValue[pin] = val;
-    return val;
-  }
-  
-  currentButtonValue[pin] = prevButtonValue[pin];
-  return prevButtonValue[pin];
+    int val = digitalRead(pin);
+    if (val == prevButtonValue[pin])
+    {
+        currentButtonValue[pin] = val;
+        return val;
+    }
+
+    delay(DEBOUNCE_DELAY);
+
+    val = digitalRead(pin);
+    if (val != prevButtonValue[pin])
+    {
+        Serial.print(pin, DEC);
+        Serial.print(" is ");
+        Serial.println(val, DEC);
+        currentButtonValue[pin] = val;
+        return val;
+    }
+
+    currentButtonValue[pin] = prevButtonValue[pin];
+    return prevButtonValue[pin];
 }
 
 boolean hasChanged(int pin)
 {
-  return prevButtonValue[pin] != currentButtonValue[pin];
+    return prevButtonValue[pin] != currentButtonValue[pin];
 }
 
 void resetButtonValues()
 {
-  for (int i=0; i < MAX_BUTTONS_INPUT; i++) {
-    prevButtonValue[i] = currentButtonValue[i];
-  }
+    for (int i = 0; i < MAX_BUTTONS_INPUT; i++)
+    {
+        prevButtonValue[i] = currentButtonValue[i];
+    }
 }
 
-void resetClockSquareFlags() {
-  for (int i = 0; i < NUM_CLOCK_SQUARES; i++) {
-    clockSquares[i] = 0;
-  }
+void resetClockSquareFlags()
+{
+    for (int i = 0; i < NUM_CLOCK_SQUARES; i++)
+    {
+        clockSquares[i] = 0;
+    }
 }
 
 void setClockSquareFlags(byte value, byte flag)
 {
-  switch (value)
-  {
+    switch (value)
+    {
     case 1:
-      switch (random(2))
-      {
+        switch (random(2))
+        {
         case 0:
-          clockSquares[0]|=flag; break;
+            clockSquares[0] |= flag;
+            break;
         case 1:
-          clockSquares[1]|=flag; break;
-      }
-      break;
+            clockSquares[1] |= flag;
+            break;
+        }
+        break;
     case 2:
-      switch (random(2))
-      {
+        switch (random(2))
+        {
         case 0:
-          clockSquares[2]|=flag; break;
+            clockSquares[2] |= flag;
+            break;
         case 1:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; break;
-      }
-      break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            break;
+        }
+        break;
     case 3:
-      switch (random(3))
-      {
+        switch (random(3))
+        {
         case 0:
-          clockSquares[3]|=flag; break;
+            clockSquares[3] |= flag;
+            break;
         case 1:
-          clockSquares[0]|=flag; clockSquares[2]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[2] |= flag;
+            break;
         case 2:
-          clockSquares[1]|=flag; clockSquares[2]|=flag; break;
-      }
-      break;
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            break;
+        }
+        break;
     case 4:
-      switch (random(3))
-      {
+        switch (random(3))
+        {
         case 0:
-          clockSquares[0]|=flag; clockSquares[3]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[3] |= flag;
+            break;
         case 1:
-          clockSquares[1]|=flag; clockSquares[3]|=flag; break;
+            clockSquares[1] |= flag;
+            clockSquares[3] |= flag;
+            break;
         case 2:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[2]|=flag; break;
-      }
-      break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            break;
+        }
+        break;
     case 5:
-      switch (random(3))
-      {
+        switch (random(3))
+        {
         case 0:
-          clockSquares[4]|=flag; break;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[2]|=flag; clockSquares[3]|=flag; break;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            break;
         case 2:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[3]|=flag; break;
-      }
-      break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            clockSquares[3] |= flag;
+            break;
+        }
+        break;
     case 6:
-      switch (random(4))
-      {
+        switch (random(4))
+        {
         case 0:
-          clockSquares[0]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[1]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[1] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 2:
-          clockSquares[0]|=flag; clockSquares[2]|=flag; clockSquares[3]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            break;
         case 3:
-          clockSquares[1]|=flag; clockSquares[2]|=flag; clockSquares[3]|=flag; break;
-      }
-      break;
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            break;
+        }
+        break;
     case 7:
-      switch (random(3))
-      {
+        switch (random(3))
+        {
         case 0:
-          clockSquares[2]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[2] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 2:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[2]|=flag; clockSquares[3]|=flag; break;
-      }
-      break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            break;
+        }
+        break;
     case 8:
-      switch (random(3))
-      {
+        switch (random(3))
+        {
         case 0:
-          clockSquares[3]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[0]|=flag; clockSquares[2]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 2:
-          clockSquares[1]|=flag; clockSquares[2]|=flag; clockSquares[4]|=flag; break;
-      }      
-      break;
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[4] |= flag;
+            break;
+        }
+        break;
     case 9:
-      switch (random(3))
-      {
+        switch (random(3))
+        {
         case 0:
-          clockSquares[0]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[1]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[1] |= flag;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 2:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[2]|=flag; clockSquares[4]|=flag; break;
-      }      
-      break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[4] |= flag;
+            break;
+        }
+        break;
     case 10:
-      switch (random(2))
-      {
+        switch (random(2))
+        {
         case 0:
-          clockSquares[2]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag; break;
-      }            
-      break;
+            clockSquares[0] |= flag;
+            clockSquares[1] |= flag;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
+        }
+        break;
     case 11:
-      switch (random(2))
-      {
+        switch (random(2))
+        {
         case 0:
-          clockSquares[0]|=flag; clockSquares[2]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag; break;
+            clockSquares[0] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
         case 1:
-          clockSquares[1]|=flag; clockSquares[2]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag; break;
-      }          
-
-      break;
-    // 12 % 12 = 0
-    case 0: case 12:
-      clockSquares[0]|=flag; clockSquares[1]|=flag; clockSquares[2]|=flag; clockSquares[3]|=flag; clockSquares[4]|=flag;
-      break;
-  }
+            clockSquares[1] |= flag;
+            clockSquares[2] |= flag;
+            clockSquares[3] |= flag;
+            clockSquares[4] |= flag;
+            break;
+        }
+        break;
+    case 12:
+        clockSquares[0] |= flag;
+        clockSquares[1] |= flag;
+        clockSquares[2] |= flag;
+        clockSquares[3] |= flag;
+        clockSquares[4] |= flag;
+        break;
+    }
 }
 
-void updateLed(int squareIndex, byte flags) {
-  CRGB color = colors[0][flags];
+void updateLed(int squareIndex, byte flags)
+{
+    CRGB color = colors[0][flags];
 
-  Serial.print("Updating LED "); Serial.print(squareIndex, DEC); Serial.print("with flag: "); Serial.println(flags, DEC);
+    Serial.print("Updating LED ");
+    Serial.print(squareIndex, DEC);
+    Serial.print("with flag: ");
+    Serial.println(flags, DEC);
 
-  switch (squareIndex) {
-    case 0: leds[9] = color; break;
-    case 1: leds[8] = color; break;
-    case 2: leds[7] = color; break;
-    case 3: leds[6] = leds[5] = color; break;
-    case 4: leds[4] = leds[3] = leds[2] = leds[1] = leds[0] = color; break;
-  }
+    switch (squareIndex)
+    {
+    case 0:
+        leds[9] = color;
+        break;
+    case 1:
+        leds[8] = color;
+        break;
+    case 2:
+        leds[7] = color;
+        break;
+    case 3:
+        leds[6] = leds[5] = color;
+        break;
+    case 4:
+        leds[4] = leds[3] = leds[2] = leds[1] = leds[0] = color;
+        break;
+    }
 }
 
-void setTime(DateTime now) {
+void setTime(DateTime now)
+{
     rtc.adjust(now);
 
     Serial.print("setting time to: ");
